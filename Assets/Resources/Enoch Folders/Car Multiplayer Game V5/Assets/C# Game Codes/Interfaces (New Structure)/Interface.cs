b7533a9c66f769d69fffb245503d8cf7b2ddcad4
enochbuugs <36535452+enochbuugs@users.noreply.github.com>
2018-11-21 13:34:15 +0000

    Interface Damage System and near final build

---
+++ Assets/Resources/Enoch Folders/Car Multiplayer Game V5/Assets/C# Game Codes/Interfaces (New Structure)/Interface.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageable
+{
+    void Damage(float damageAmount);
+    float GetDamage { get; set; }
+}
+
+public interface IPowerUpUse
+{
+    void TriggerPowerup(string powerUp);
+
+    //Powerup logic just call this interface, then destroy or hideselves
+}
+
---
+++ Assets/Resources/Enoch Folders/Car Multiplayer Game V5/Assets/C# Game Codes/Old Structure/UI/EnemyDamageMeter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyDamageMeter : MonoBehaviour //IDamageable
+{
+
+    public Text enemyCarHealth;
+    public GameObject mainPlayer;
+    public float currentEnemyCarHealth;
+    public float maxEnemyCarHealth = 100;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentEnemyCarHealth = maxEnemyCarHealth; // set the current health to max health
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ShowCarHealth();
+        CarWreaked();
+    }
+
+    void ShowCarHealth()
+    {
+        // show the car health
+        enemyCarHealth.text = "Enemy Car Health: " + (int)currentEnemyCarHealth + "%";
+    }
+
+    void CarWreaked()
+    {
+        if (currentEnemyCarHealth <= 0)
+        {
+            currentEnemyCarHealth = 0;
+            Debug.Log("You are wreaked");
+            //can write more logic here to respawn 
+            //back to lobby or start again...
+        }
+    }
+
+    //Implement the Damage function of IDamageable
+    public void Damage(float damageAmount)
+    {
+        currentEnemyCarHealth -= damageAmount;
+    }
+}
