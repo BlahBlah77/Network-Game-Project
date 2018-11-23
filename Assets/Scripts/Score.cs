b69b4bd7d8b8dc8c98984c62b30099a9811ba675
BenWoolf <K1637149@kingston.ac.uk>
2018-11-23 13:59:57 +0000

    Arena with Score,Timer, Heatlh Bar Scripts

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	// Sets up variables for current score "scoreValue" and MaxScore (Text included)
+	public static int scoreValue = 0;
+	public static int MaxScore = 70;
+	Text score;
+
+	// Set up text to show score value
+	void Start()
+	{
+		score = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		ScoreValue ();
+
+	}
+
+	// Displays Score text and Updates it
+	public void ScoreValue ()
+	{
+		score.text = "Score: " + scoreValue;
+	}
+}
