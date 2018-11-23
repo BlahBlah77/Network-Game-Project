83924730b79392526b5c665ea5cbec5b51aecaec
BlahBlah77 <k1623759@kingston.ac.uk>
2018-11-23 02:38:05 +0000

    Network code

---
+++ Assets/Resources/Luke Folders/Scripts/Test Scripts/Pong Test/Player_Pad.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Pad : Photon.MonoBehaviour {
+
+	public float speed = 5f;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (photonView.isMine) 
+		{
+			InputMovement ();
+		}
+	}
+
+	void InputMovement ()
+	{
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			rb.MovePosition (rb.position + Vector3.up * speed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			rb.MovePosition (rb.position - Vector3.up * speed * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Resources/Luke Folders/Scripts/Test Scripts/Pong Test/Pong_Ball.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pong_Ball : Photon.MonoBehaviour {
+
+	public float startSpeed = 5f;
+	public float maxSpeed = 20f;
+	public float speedIncrease = 0.25f;
+	private float currentSpeed;
+	private Vector2 currentDir;
+
+	// Use this for initialization
+	void Start () 
+	{
+		currentSpeed = startSpeed;
+		currentDir = Random.insideUnitCircle.normalized;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (PhotonNetwork.playerList.Length == 0) 
+		{
+			return;
+		}
+		Vector2 moveDir = currentDir * currentSpeed * Time.deltaTime;
+		transform.Translate (new Vector3 (moveDir.x, moveDir.y, 0f));
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Boundary") 
+		{
+			currentDir.y *= -1;
+		} 
+		else if (other.tag == "Player") 
+		{
+			currentDir.x *= -1;
+		} 
+		else if (other.tag == "Goal") 
+		{
+			ChangeColourTo (new Vector3 (Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f)));
+			ChangePositionTo (new Vector3 (0f, 1.5f, -2f));
+			ChangeDirTo (Random.insideUnitCircle.normalized);
+		}
+	}
+
+	[PunRPC]
+	void ChangePositionTo(Vector3 myPosition)
+	{
+		GetComponent<Transform> ().position = myPosition;
+		if (photonView.isMine)
+			photonView.RPC ("ChangePositionTo", PhotonTargets.OthersBuffered, myPosition);
+	}
+
+	[PunRPC]
+	void ChangeDirTo(Vector3 myDirection)
+	{
+		currentDir = myDirection;
+		if (photonView.isMine)
+			photonView.RPC ("ChangeDirTo", PhotonTargets.OthersBuffered, myDirection);
+	}
+
+	[PunRPC]
+	void ChangeColourTo(Vector3 color)
+	{
+		GetComponent<Renderer> ().material.color = new Color (color.x, color.y, color.z, 1f);
+		if (photonView.isMine)
+			photonView.RPC ("ChangeColourTo", PhotonTargets.OthersBuffered, color);
+	}
+}
